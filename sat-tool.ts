87e9932949ef07b104dfc2da67001a3d79d3ee5b
Tito Mitto <mordeccai9@gmail.com>
2022-02-10 11:51:23 +0300

    initial commit

---
+++ sat-tool.ts
@@ -0,0 +1,14 @@
+import { parse } from "https://deno.land/std/flags/mod.ts";
+import { LocalStorage } from "https://deno.land/x/storage@0.0.5/mod.ts"
+import {  ServiceUtil } from "./services.ts";
+
+const args = parse(Deno.args);
+const storage = new LocalStorage<string>();
+const providedPath = args.path ?? storage.get("path") ?? ".";
+const projectPath = `${providedPath}/lib`
+
+storage.set("path", providedPath)
+storage.save()
+
+
+new ServiceUtil(args, projectPath);
